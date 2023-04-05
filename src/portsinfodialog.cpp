cfe2352ee988620107f4c1fca1e6d85b8dd3f450
Ilya Sotnikov <ilyasotnikov45@gmail.com>
2023-04-05 11:15:15 +0500

    Refactor small widgets

--- src/portsinfodialog.cpp
+++ src/portsinfodialog.cpp
@@ -5,35 +5,32 @@
 #include <qscrollarea.h>
 #include <qserialportinfo.h>
 
-/**
- * @brief Constructs a new PortsInfoDialog object
- *
- * @param parent
- */
+using namespace Qt::Literals::StringLiterals;
+
 PortsInfoDialog::PortsInfoDialog(QWidget *parent) : QDialog{ parent }
 {
-    auto scroll{ new QScrollArea{ this } };
-    auto widget{ new QWidget{ this } };
+    const auto scroll{ new QScrollArea{ this } };
+    const auto widget{ new QWidget{ this } };
     scroll->setWidget(widget);
     scroll->setWidgetResizable(true);
-    auto layout{ new QVBoxLayout{ widget } };
+    const auto layout{ new QVBoxLayout{ widget } };
 
     const auto portsInfo{ QSerialPortInfo::availablePorts() };
     for (const auto &info : portsInfo) {
-        QString infoStr{
-            "Port: " + info.portName() + "\n" + "Location: " + info.systemLocation() + "\n"
-            + "Manufacturer: " + info.manufacturer() + "\n"
-            + "Serial number: " + info.serialNumber() + "\n" + "Vendor identifier: "
-            + (info.hasVendorIdentifier() ? QString::number(info.vendorIdentifier(), 16) : "")
-            + "\n" + "Product identifier: "
-            + (info.hasProductIdentifier() ? QString::number(info.productIdentifier(), 16) : "")
-            + "\n"
+        const QString infoStr{
+            u"Port: "_s + info.portName() + u"\n"_s + u"Location: "_s + info.systemLocation()
+            + u"\n"_s + u"Manufacturer: "_s + info.manufacturer() + u"\n"_s + u"Serial number: "_s
+            + info.serialNumber() + u"\n"_s + u"Vendor identifier: "_s
+            + (info.hasVendorIdentifier() ? QString::number(info.vendorIdentifier(), 16) : u""_s)
+            + u"\n"_s + u"Product identifier: "_s
+            + (info.hasProductIdentifier() ? QString::number(info.productIdentifier(), 16) : u""_s)
+            + u"\n"_s
         };
-        auto label{ new QLabel{ infoStr, this } };
+        const auto label{ new QLabel{ infoStr, this } };
         layout->addWidget(label);
     }
 
-    auto dialogLayout{ new QVBoxLayout{ this } };
+    const auto dialogLayout{ new QVBoxLayout{ this } };
     dialogLayout->addWidget(scroll);
-    setWindowTitle("Ports info");
+    setWindowTitle(u"Ports info"_s);
 }
