2d01cdf5eb4a2d76eba419b0598fc6dc1fd5eb6b
Ilya Sotnikov <ilyasotnikov45@gmail.com>
2023-04-05 08:31:57 +0500

    Fix a bug regarding appending the last N values to the spectrum

--- src/chartdatacontainer.cpp
+++ src/chartdatacontainer.cpp
@@ -23,44 +23,52 @@ void ChartDataContainer::clear()
  */
 void ChartDataContainer::append(const QList<qreal> &data, bool appendToPlot, bool appendToSpectrum)
 {
-    if (appendToPlot)
-        appendPlot(data);
-    if (appendToSpectrum)
-        appendSpectrum(data);
+    // the last spectrum values are recreated from the last plot values
+    appendPlot(data, appendToPlot, appendToPlot || appendToSpectrum);
+    appendSpectrum(data, appendToSpectrum, appendToSpectrum);
 }
 
 /**
  * @brief Appends data to the spectrum data container
  *
  * @param data Data to be appended
- * @param keys Append to these keys
- * @param values Append to these values
+ * @param append
+ * @param appendToLast
+ *
  */
-void ChartDataContainer::appendSpectrum(const QList<qreal> &data)
+void ChartDataContainer::appendSpectrum(const QList<qreal> &data, bool append, bool appendToLast)
 {
-    for (auto x : data) {
-        auto it{ std::lower_bound(spectrumData->begin(), spectrumData->end(),
-                                  QCPGraphData::fromSortKey(x), qcpLessThanSortKey<QCPGraphData>) };
-        if ((it != spectrumData->end()) && (it->mainKey() == x))
-            ++it->value;
-        else
-            spectrumData->add(QCPGraphData{ x, 1 });
-    }
-
-    if (lastPointsCount <= 0)
+    if (!append && !appendToLast)
         return;
 
-    spectrumDataLast->clear();
+    if (append) {
+        for (auto x : data) {
+            auto it{ std::lower_bound(spectrumData->begin(), spectrumData->end(),
+                                      QCPGraphData::fromSortKey(x),
+                                      qcpLessThanSortKey<QCPGraphData>) };
+            if ((it != spectrumData->end()) && (it->mainKey() == x))
+                ++it->value;
+            else
+                spectrumData->add(QCPGraphData{ x, 1 });
+        }
+    }
 
-    for (auto point : *plotDataLast) {
-        auto value{ point.mainValue() };
-        auto it{ std::lower_bound(spectrumDataLast->begin(), spectrumDataLast->end(),
-                                  QCPGraphData::fromSortKey(value),
-                                  qcpLessThanSortKey<QCPGraphData>) };
-        if ((it != spectrumDataLast->end()) && (it->mainKey() == value))
-            ++it->value;
-        else
-            spectrumDataLast->add(QCPGraphData{ value, 1 });
+    if (appendToLast) {
+        if (lastPointsCount <= 0)
+            return;
+
+        spectrumDataLast->clear();
+
+        for (auto point : *plotDataLast) {
+            auto value{ point.mainValue() };
+            auto it{ std::lower_bound(spectrumDataLast->begin(), spectrumDataLast->end(),
+                                      QCPGraphData::fromSortKey(value),
+                                      qcpLessThanSortKey<QCPGraphData>) };
+            if ((it != spectrumDataLast->end()) && (it->mainKey() == value))
+                ++it->value;
+            else
+                spectrumDataLast->add(QCPGraphData{ value, 1 });
+        }
     }
 }
 
@@ -75,17 +83,29 @@ void ChartDataContainer::setRawSpectrumData(const QCPGraphDataContainer &rawData
  *
  * @param data Data to be appended
  * @param keys Append to these keys
- * @param values Append to these values
+ * @param append
+ * @param appendToLast
+ *
  */
-void ChartDataContainer::appendPlot(const QList<qreal> &data)
+void ChartDataContainer::appendPlot(const QList<qreal> &data, bool append, bool appendToLast)
 {
+    if (!append && !appendToLast)
+        return;
+
     const auto dataSize{ data.size() };
-    const auto currentDataSize{ plotData->size() };
+    static qsizetype currentDataSize{ 0 };
     for (qsizetype i{ 0 }; i < dataSize; ++i) {
         auto key{ static_cast<double>(i + currentDataSize) };
         auto value{ data.at(i) };
-        plotData->add(QCPGraphData{ key, value });
-        plotDataLast->add(QCPGraphData{ key, value });
+
+        if (append)
+            plotData->add(QCPGraphData{ key, value });
+
+        if (appendToLast)
+            plotDataLast->add(QCPGraphData{ key, value });
+
+        if (append || appendToLast)
+            ++currentDataSize;
     }
 
     if (lastPointsCount <= 0)
