930da3a896f70e4e5cf4b50fccffaeaf4aef06ef
Ilya Sotnikov <ilyasotnikov45@gmail.com>
2023-04-07 17:55:08 +0500

    Add windows deployment

---
+++ deploy/windows/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+name="qUART"
+
+deploy_dir=`pwd`
+
+mkdir -p ${name}
+
+mkdir -p ../../build-release
+cd ../../build-release
+cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+
+cp ${name}.exe ${deploy_dir}/${name}
+cd ${deploy_dir}/${name}
+windeployqt-qt6 ${name}.exe
+ldd ${name}.exe | grep -Eiv 'system32|winsxs' | awk '{print $3}' | xargs cp -u -t .
+if [ -f "${name}.zip" ]; then
+    7z u ${deploy_dir}/${name}.zip *
+else
+    7z a ${deploy_dir}/${name}.zip *
+fi
+
+cd ${deploy_dir}
