4d0ca22caeaf8e6a5137803cbeb4ce11dd789501
Ilya Sotnikov <ilyasotnikov45@gmail.com>
2023-04-07 18:17:23 +0500

    Add new release creation to retag.sh

--- deploy/retag.sh
+++ deploy/retag.sh
@@ -4,3 +4,5 @@ git tag -d latest
 git push --delete origin latest
 git tag latest
 git push origin latest
+
+firefox 'https://github.com/ilya-sotnikov/qUART/releases/new?tag=latest&title=Latest&body=Latest+release'
