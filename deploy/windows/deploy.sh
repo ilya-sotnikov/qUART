aab648b66ae0cbeed8e94c3d581a6232a25d8758
Ilya Sotnikov <ilyasotnikov45@gmail.com>
2023-04-08 09:40:48 +0500

    fix: windows deployment archive updating

--- deploy/windows/deploy.sh
+++ deploy/windows/deploy.sh
@@ -14,7 +14,7 @@ cp ${name}.exe ${deploy_dir}/${name}
 cd ${deploy_dir}/${name}
 windeployqt-qt6 ${name}.exe
 ldd ${name}.exe | grep -Eiv 'system32|winsxs' | awk '{print $3}' | xargs cp -u -t .
-if [ -f "${name}.zip" ]; then
+if [ -f "${deploy_dir}/${name}-windows_x86-64.zip" ]; then
     7z u ${deploy_dir}/${name}-windows_x86-64.zip *
 else
     7z a ${deploy_dir}/${name}-windows_x86-64.zip *
